1bfa3ea064450de591c171bcaaa9d30bd69cda5b
Rafael Marra <rafaelmarrasilva@gmail.com>
2022-03-09 13:36:45 -0300

    projeto Calculadora

---
+++ 01_RestWithASPNET_Calculadora/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private double ConvertToDecimal(string numero)
+        {
+            if (double.TryParse(numero, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out double numValido))
+                return numValido;
+            return 0;
+        }
+
+        private bool IsNumeric(string numero)
+        {
+            return double.TryParse(numero, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out double numValido);
+        }
+    }
+}
